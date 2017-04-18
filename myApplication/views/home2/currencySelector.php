68d3dfdf27e968a5b6ae24d1d3e6fede101d1343
mhrezaei <mr.mhrezaei@gmail.com>
2017-04-18 10:52:21 +0430

    bug fix

--- myApplication/views/home2/currencySelector.php
+++ myApplication/views/home2/currencySelector.php
@@ -4,6 +4,8 @@
 		</option>
 	<?php foreach($currencies as $currency => $rates) { ?>
 		<?php if($rates[1]<0 OR $rates[2]<0) continue; ?>
+        <?php if (strlen($rates[4])){ ?>
 		<option value="<?php echo $currency; ?>" ><?php echo $rates[4]; ?> (<?php echo $currency; ?>)</option>
+	<?php } ?>
 	<?php } ?>
 </select>
